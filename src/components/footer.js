489ae8838028380d19e8c89ae7644c1549569ab4
Akash-Potti <akashpotti07@gmail.com>
2023-05-26 19:33:50 +0530

    second commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const Footer = () => {
     return <div className="py-4 text-center bg-primary text-white">
-        &copy; 2023 Akash Potti.cd All Rights Reserved
+        &copy; 2023 Akash Potti All Rights Reserved
     </div>
 
 };
